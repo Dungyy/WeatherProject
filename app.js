841305e8b5dd41d26141a5411368a4f5f02a6b88
Dungyy <erickmunoz13@gmail.com>
2021-11-14 21:04:07 -0600

    added index.HTML app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const express = require("express");
+
+const app = express();
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+app.listen(4000, function(){
+    console.log("SERVER RUNNING ON PORT 4000")
+});
