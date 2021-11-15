cedc46d0934800ebfff7824992edf88b66b8fa09
Dungyy <erickmunoz13@gmail.com>
2021-11-14 23:28:13 -0600

    Fully working API weather Request

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 
 const express = require("express");
 const https = require("https");
+const bodyPaser = require("body-parser");
+
 const app = express();
 
+app.use(bodyPaser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Minnesota&appid=7aa471e466f99787941e1727bb31a222&units=imperial";
-    
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const apiKey = "7aa471e466f99787941e1727bb31a222"
+    const units = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query + "&appid=" + apiKey +"&units=" + units;
+
     https.get(url, function(response){
         console.log(response.statusCode);
 
@@ -15,13 +24,16 @@ app.get('/', function(req, res){
             const weatherData = JSON.parse(data)
             const weatherDescription = weatherData.weather[0].description
             const temp = weatherData.main.temp
-            res.write("<h1>The weather Description is: " + weatherDescription + "</h1>");
-            res.write("<h1>The Temp in Minnesota is " + temp + " Degrees Celcius.</h1>");
+            const icon = weatherData.weather[0].icon
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<h1>The Weather in " + query + " is: " + weatherDescription + "</h1>");
+            res.write("<h2>The Temp in " + query + " is: " + temp + " Degrees Celcius</h2>");
+            res.write("<img src=" + imageURL + ">");
             res.send()
         })
     })
 })
-
+ 
 
 app.listen(4000, function(){
     console.log("SERVER RUNNING ON PORT 4000")
